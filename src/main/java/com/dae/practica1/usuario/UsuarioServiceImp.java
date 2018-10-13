3dff60113187a8d5540954e4ff4f6c66f32b3370
Pedro Luis <p.luis_4@hotmail.com>
2018-10-13 11:41:41 +0200

    identifica usuario, crea evento

--- src/main/java/com/dae/practica1/usuario/UsuarioServiceImp.java
+++ src/main/java/com/dae/practica1/usuario/UsuarioServiceImp.java
@@ -20,25 +20,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class UsuarioServiceImp implements UsuarioService {
 
-    Map<String,Usuario> usuarios;
-    
-    public UsuarioServiceImp(){
+    private Map<String, Usuario> usuarios;
+    private int idUsuario = 0;
+    private int token;
+
+    public UsuarioServiceImp() {
         usuarios = new TreeMap<>();
-        usuarios.put("yosiph", new Usuario("yosiph","yosiph","Jose",new Date()));
+        usuarios.put("yosiph", new Usuario(idUsuario++, "yosiph", "yosiph", "Jose", new Date()));
+        token = -1;
     }
-    
+
     @Override
     public boolean RegistraUsuario(String usuario, String password, String nombre, Date fNac) {
-        Usuario aux=new Usuario(usuario,nombre,password,fNac);
-        Usuario puti=usuarios.put(usuario,aux);
+
+        Usuario aux = new Usuario(idUsuario++, usuario, nombre, password, fNac);
+        Usuario puti = usuarios.put(usuario, aux);
         return puti != null;
     }
 
     @Override
-    public void IdentificaUsuario(String usuario, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int IdentificaUsuario(String usuario, String password) {
+        int identificado = -1;
+        if (usuarios.get(usuario) != null) {
+            if (usuarios.get(usuario).getPassword().equals(password)) {
+                identificado = token = usuarios.get(usuario).getIdUsuario();
+                
+            }
+        }
+        return identificado;
     }
-
+    
     @Override
     public List<Evento> ListaEventosInscritos(String usuario) {
         return usuarios.get(usuario).getEventosInscritos();
@@ -55,10 +66,10 @@ public class UsuarioServiceImp implements UsuarioService {
     }
 
     @Override
-    public Map<String,Usuario> usuariosRegistrados() {
-        
+    public Map<String, Usuario> usuariosRegistrados() {
+
         return usuarios;
-        
+
     }
-    
+
 }
